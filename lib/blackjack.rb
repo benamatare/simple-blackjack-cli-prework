1c4900343228b3a87552d04aae9ae5ff4e23137a
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 03:37:18 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -44,16 +44,6 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-#def runner
-#  welcome
-#  card_total = initial_round
-#    while card_total < 21
-#      card_total = hit?(card_total)
-#      card_total = display_card_total(card_total)
-#    end
-#    end_game(card_total)
-#end
-
 def runner
   welcome
   card_total = initial_round
